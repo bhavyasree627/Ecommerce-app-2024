2c0f079ca8cb7a2061d0c6bd0c1cae36db0eda79
Bhavya <bhogathibhavya99@gmail.com>
2024-03-25 13:09:45 +0530

    Learnt using nodemon dotenv server

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+//configure env
+dotenv.config();
+//rest object
+const app = express()
+
+//rest api
+app.get('/', (req, res) => {
+    res.send('<h1>This is Ecommerce</h1>')
+})
+
+//PORT 
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`.bgCyan.white);
+})
