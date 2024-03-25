cf6ae077cc6e080f4ba0d15f2463f80ee5853a2c
Bhavya <bhogathibhavya99@gmail.com>
2024-03-25 18:51:12 +0530

    mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from "./config/db.js";
+
 //configure env
 dotenv.config();
+
+//database config
+connectDB();
+
 //rest object
 const app = express()
+
+//middlewares
+app.use(morgan('dev')); //log requests to the console
+app.use(express.json()); //parse incoming requests of content type application/json
 
 //rest api
 app.get('/', (req, res) => {
